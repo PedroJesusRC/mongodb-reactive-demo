99d0c0ba3e4be610cd167858732bfd147d79eb6f
Pedro Jesus Rodriguez <pedro.rodriguez@in2.es>
2022-01-13 12:39:02 +0100

    Added project

---
+++ src/main/java/com/pjrc/mongodbreactivedemo/MongodbReactiveDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pjrc.mongodbreactivedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbReactiveDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbReactiveDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pjrc/mongodbreactivedemo/MongodbReactiveDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjrc.mongodbreactivedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbReactiveDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
